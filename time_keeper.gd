c86246fda4a159598aa9008b6198ef520f36f5b9
Michael Campbell <rainbow@portal2d.xyz>
2024-11-12 17:07:48 -0500

    fix: broken time formatting

--- time_keeper.gd
+++ time_keeper.gd
@@ -8,7 +8,8 @@ func reset() -> void:
 	_end = -1
 
 func stop() -> void:
-	_end = Time.get_ticks_msec()
+	if _end == -1:
+		_end = Time.get_ticks_msec()
 
 func get_duration() -> String:
 	var end := _end if _end != -1 else Time.get_ticks_msec()
@@ -17,6 +18,6 @@ func get_duration() -> String:
 	var hours := int(delta / (1000 * 60 * 60)) # 1000 ms * 60 s * 60 m
 	var minutes := int(delta / (1000 * 60)) % 60 # 1000 ms * 60 s % 60 m
 	var seconds := int(delta / 1000) % 60 # 1000 ms % 60 s
-	var millis := int(delta) % 60
+	var millis := int(delta) % 1000
 
 	return "Time: %02d:%02d:%02d.%03d" % [hours, minutes, seconds, millis]
